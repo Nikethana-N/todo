43eec6c97589c00dc3409f92f0b5331074d76b12
Nikethana-N <49244102+Nikethana-N@users.noreply.github.com>
2020-01-07 16:41:36 +0530

    Update send-sms.rb

--- config/locales/send-sms.rb
+++ config/locales/send-sms.rb
@@ -1,13 +1,1 @@
-require("bundler")
-Bundler.require()
 
-account_sid = "AC24a376b39dba181cc733c4615d9ee93c"
-auth_token = "84e42c18bae7727b7a70fab8dbd8d1a6"
-
-@client = Twilio::REST::Client.new(account_sid,auth_token)
-
-@client.messages.creat(
-	to: "+919566275650"
-	from: ""#number you need to purchase
-	body: "My first sms to phone"
-	)
